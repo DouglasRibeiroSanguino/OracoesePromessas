be2cfa3c678f267ee001eb69d6a4255d91af3108
perkles <douglasribeirosanguino3@gmail.com>
2022-01-12 21:58:18 -0300

    First Commit

---
+++ app/src/main/java/oracoes/e/promessas/MainActivity.kt
@@ -0,0 +1,19 @@
+package oracoes.e.promessas
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    btn_santa_rita.setOnClickListener {
+        val i = Intent(this, Pedido_a_Santa_Rita::class.java)
+        startActivity(i)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
---
+++ app/src/main/java/oracoes/e/promessas/Pedido_a_Santa_Rita.kt
@@ -0,0 +1,11 @@
+package oracoes.e.promessas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pedido_a_Santa_Rita : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pedido_a_santa_rita)
+    }
+}
