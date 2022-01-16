593a44438387de5e73c7ba7d63d9468002f22a21
perkles <douglasribeirosanguino3@gmail.com>
2022-01-16 12:06:55 -0300

    New updates:
    Add SplashScreenActivity
    Add New Icon launcher
    Reorganized AndroidManifest
    And improved the texts from Activities

---
+++ app/src/main/java/oracoes/e/promessas/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package oracoes.e.promessas
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        iv_first.alpha = 0f
+        iv_first.animate().setDuration(2500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
